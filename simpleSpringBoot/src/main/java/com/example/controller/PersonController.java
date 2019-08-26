27f7d13151675af6a9876983195949f2a9a59e39
Shafat  Saeed <shafat.saeed@gmail.com>
2019-08-26 22:43:13 +0100

    Adding scan annotations
    Adding scan annotations

--- simpleSpringBoot/src/main/java/com/example/controller/PersonController.java
+++ simpleSpringBoot/src/main/java/com/example/controller/PersonController.java
@@ -1,9 +1,9 @@
 package com.example.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +17,7 @@ public class PersonController {
 	@Autowired
 	PersonService personService;
 	
-	@RequestMapping("/all")
+	 @GetMapping("/all")
 	public Iterable<Employee> getAll(){
 		return personService.getAll();
 	}
--- simpleSpringBoot/src/main/java/com/example/simpleSpringBoot/SimpleSpringBootApplication.java
+++ simpleSpringBoot/src/main/java/com/example/simpleSpringBoot/SimpleSpringBootApplication.java
@@ -3,9 +3,14 @@ package com.example.simpleSpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EntityScan(basePackages={"com.example.model"})
+@ComponentScan(basePackages= {"com.example"})
+@EnableJpaRepositories("com.example.Respository")
 public class SimpleSpringBootApplication {
 
 	public static void main(String[] args) {
